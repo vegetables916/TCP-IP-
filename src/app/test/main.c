9baa78ac79887b28d405a9a4d0fc56542463ba04
SteadmanYan <2845422834@qq.com>
2026-07-21 23:14:24 +0800

    数据结构（链表）的设计

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -15,6 +15,7 @@
  #include "echo/tcp_echo_server.h"
  #include "net.h"
  #include "dbg.h"
+ #include "nlist.h"
 
  static sys_sem_t sem;
  static sys_mutex_t mutex;
@@ -89,15 +90,81 @@ net_err_t netdev_init(void) {
     return NET_ERR_OK;
 }
 
+typedef struct _tnode_t {
+    int id;
+    nlist_node_t node;
+}tnode_t;
+
+void nlist_test() {
+    #define NODE_CNT 4
+    tnode_t node[NODE_CNT];
+    nlist_t list;
+
+    // 测试首部操作
+    nlist_init(&list);
+    for(int i = 0; i < NODE_CNT; i++) {
+        node[i].id = i;
+        nlist_insert_first(&list, &node[i].node);
+    }
+
+    plat_printf("insert first\n");
+    
+    nlist_node_t * p;
+    nlist_for_each(p, &list) {
+        tnode_t * tnode = nlist_entry(p, tnode_t, node);
+        plat_printf("id: %d\n", tnode->id);
+    }
+
+    plat_printf("remove first\n");
+    for(int i = 0; i < NODE_CNT; i++) {
+        p = nlist_remove_first(&list);
+        plat_printf("id: %d\n", nlist_entry(p, tnode_t, node)->id);
+    }
+
+    // 测试尾部操作
+    for(int i = 0; i < NODE_CNT; i++) {
+        nlist_insert_last(&list, &node[i].node);
+    }
+
+    plat_printf("insert last\n");
+    nlist_for_each(p, &list) {
+        tnode_t * tnode = nlist_entry(p, tnode_t, node);
+        plat_printf("id: %d\n", tnode->id);
+    }
+
+    plat_printf("remove last\n");
+    for(int i = 0; i < NODE_CNT; i++) {
+        p = nlist_remove_last(&list);
+        plat_printf("id: %d\n", nlist_entry(p, tnode_t, node)->id);
+    }
+
+    // 测试插入任意位置操作
+    plat_printf("insert after\n");
+    for(int i = 0; i < NODE_CNT; i++) {
+        nlist_insert_after(&list, nlist_first(&list), &node[i].node);
+    }
+
+    nlist_for_each(p, &list) {
+        tnode_t * tnode = nlist_entry(p, tnode_t, node);
+        plat_printf("id: %d\n", tnode->id);
+    }
+}
+
+void basic_test(void) {
+    nlist_test();
+}
+
 #define DBG_TEST DBG_LEVEL_INFO
  int main(void) {
     dbg_info(DBG_TEST, "info");
     dbg_warning(DBG_TEST, "warning");
     dbg_error(DBG_TEST, "error");
-    dbg_assert(1 == 1, "failed");
-    dbg_assert(1 == 0, "failed");
+    // dbg_assert(1 == 1, "failed");
+    // dbg_assert(1 == 0, "failed");
     
     net_init();
+
+    basic_test();
 
     net_start();
 
